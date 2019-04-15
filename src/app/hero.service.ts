b926b5197c847181b56d49dc1515d6cd05bfd9bf
Axel <ax.fiolle@gmail.com>
2019-04-15 11:23:41 +0200

    Step5
    + ng generate module app-routing
    + ng generate component dashboard

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,9 +8,15 @@ import { HEROES } from './mock-heroes';
   providedIn: 'root'
 })
 export class HeroService {
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
