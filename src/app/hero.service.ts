534878c0118bcb9497bc2b3bb7029f1865b6ca0f
Axel <ax.fiolle@gmail.com>
2019-04-15 10:50:40 +0200

    Step4
    + ng generate component messages
    + ng generate service message

---
+++ src/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from './message.service';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+  constructor(private messageService: MessageService) { }
+}
